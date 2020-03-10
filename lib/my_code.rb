958755dbf5eadb18fdac86ed4d80ce009b8d8741
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-10 14:17:02 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # Your Code Here
+
+
+def map(array)
+  result = []
+  i = 0 
+  while i < array.length do 
+    b = yield(array[i])
+    result << b
+    i += 1 
+  end 
+  result 
+end 
+
+def reduce(array, start = false)
+  if start 
+    sum = start
+    i = 0 
+  else 
+    sum = array[0]
+    i = 1 
+  end 
+  
+  while i < array.length do 
+    sum = yield(sum, array[i])
+    i += 1 
+  end
+  sum
+end 
